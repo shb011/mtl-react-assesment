3b5903003975c95cb1df060422f71f45e81b2d39
shb011 <sazzad.bhuiyan011@gmail.com>
2022-06-20 09:07:21 +0600

    Product details page

--- src/pages/ProductDescription.js
+++ src/pages/ProductDescription.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import data from '../db.json';
 
 export default function ProductDescription() {
@@ -10,7 +10,47 @@ export default function ProductDescription() {
   console.log('products', data.products);
   return (
     <div>
-      <h1>{product.name}</h1>
+      <div className='back-to-result'>
+        <Link to='/'>Back to result</Link>
+      </div>
+      <div className='details'>
+        <div className='details-image'>
+          <img src={product.image} alt='product'></img>
+        </div>
+        <div className='details-info'>
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>4.5 Stars (50 Reviews)</li>
+            <li>
+              Price: <b>৳ {product.price}</b>
+            </li>
+            <li>
+              Description:
+              <div>{product.description}</div>
+            </li>
+          </ul>
+        </div>
+        <div className='details-action'>
+          <ul>
+            <li>Price: ৳ {product.price}</li>
+            <li>Status: Instock</li>
+            <li>
+              Qty:
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </li>
+            <li>
+              <button className='button primary'>Add to Cart</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
